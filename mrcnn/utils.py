d21f739616a1fb7d91b0a23c156baad58a72b3a6
abhtri2009 <abhtri2009@gmail.com>
2019-06-05 01:59:00 +0530

    read onyl the first frame of the image.

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -356,7 +356,7 @@ class Dataset(object):
         """Load the specified image and return a [H,W,3] Numpy array.
         """
         # Load image
-        image = skimage.io.imread(self.image_info[image_id]['path'])
+        image = skimage.io.imread(self.image_info[image_id]['path'],img_num=0)
         # If grayscale. Convert to RGB for consistency.
         if image.ndim != 3:
             image = skimage.color.gray2rgb(image)
